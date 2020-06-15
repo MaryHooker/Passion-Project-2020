9237aa50783f2ec3a1076f0e106e728a539a2a9e
MaryHooker <mc020791@yahoo.com>
2020-06-15 12:03:12 -0500

    updated type property in dream model to be enumerated

--- dreamer/server/models/DreamSchema.js
+++ dreamer/server/models/DreamSchema.js
@@ -6,10 +6,12 @@ let Schema = mongoose.Schema;
 //Create new instance
 let DreamSchema = new Schema(
     {
-        type:{required:true,type:String},
+        type:{required:true,type:String, enum:['Whatever','Daydream','Lucid','Nightmare','Recurring','Healing','Prophetic','Signal','Epic']},
         dreamDescription:{required:true,type:String},
         dreamer:[{type:mongoose.Schema.Types.ObjectId,ref:"dreamers"}],
-        date:{type:Date, default:Date.now}
+        date:{type:Date, default:Date.now},
+        meanings:[],
+
     }
 )
 

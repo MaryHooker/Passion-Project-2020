94f0a071244c9caea7b38a9eefa3b8c409aa1005
MaryHooker <mc020791@yahoo.com>
2020-06-25 12:11:16 -0500

    ability for user to read all meanings by letter

--- dreamer/dreamer/src/components/Customer/Home/CustomerHome.jsx
+++ dreamer/dreamer/src/components/Customer/Home/CustomerHome.jsx
@@ -33,7 +33,34 @@ class CustomerHome extends Component {
                 <div className='dreamerMeaningsLinkP'>
                     <h4>Meanings</h4>
                 <Link to='/dreamer/meanings' className='dreamersLink'><span className='meaningsAll'>All</span></Link>
-                <Link to='/meanings/letter/letter'>A</Link>
+                <br/>
+                <br/>
+                <Link to={`/meanings/letter/${'A'}`} className='noLineLinks'><span className='meaningLetters'>A</span></Link>
+                <Link to={`/meanings/letter/${'B'}`} className='noLineLinks'><span className='meaningLetters'>B</span></Link>
+                <Link to={`/meanings/letter/${'C'}`} className='noLineLinks'><span className='meaningLetters'>C</span></Link>
+                <Link to={`/meanings/letter/${'D'}`} className='noLineLinks'><span className='meaningLetters'>D</span></Link>
+                <Link to={`/meanings/letter/${'E'}`} className='noLineLinks'><span className='meaningLetters'>E</span></Link>
+                <Link to={`/meanings/letter/${'F'}`} className='noLineLinks'><span className='meaningLetters'>F</span></Link>
+                <Link to={`/meanings/letter/${'G'}`} className='noLineLinks'><span className='meaningLetters'>G</span></Link>
+                <Link to={`/meanings/letter/${'H'}`} className='noLineLinks'><span className='meaningLetters'>H</span></Link>
+                <Link to={`/meanings/letter/${'I'}`} className='noLineLinks'><span className='meaningLetters'>I</span></Link>
+                <Link to={`/meanings/letter/${'J'}`} className='noLineLinks'><span className='meaningLetters'>J</span></Link>
+                <Link to={`/meanings/letter/${'K'}`} className='noLineLinks'><span className='meaningLetters'>K</span></Link>
+                <Link to={`/meanings/letter/${'L'}`} className='noLineLinks'><span className='meaningLetters'>L</span></Link>
+                <Link to={`/meanings/letter/${'M'}`} className='noLineLinks'><span className='meaningLetters'>M</span></Link>
+                <Link to={`/meanings/letter/${'N'}`} className='noLineLinks'><span className='meaningLetters'>N</span></Link>
+                <Link to={`/meanings/letter/${'O'}`} className='noLineLinks'><span className='meaningLetters'>O</span></Link>
+                <Link to={`/meanings/letter/${'P'}`} className='noLineLinks'><span className='meaningLetters'>P</span></Link>
+                <Link to={`/meanings/letter/${'Q'}`} className='noLineLinks'><span className='meaningLetters'>Q</span></Link>
+                <Link to={`/meanings/letter/${'R'}`} className='noLineLinks'><span className='meaningLetters'>R</span></Link>
+                <Link to={`/meanings/letter/${'S'}`} className='noLineLinks'><span className='meaningLetters'>S</span></Link>
+                <Link to={`/meanings/letter/${'T'}`} className='noLineLinks'><span className='meaningLetters'>T</span></Link>
+                <Link to={`/meanings/letter/${'U'}`} className='noLineLinks'><span className='meaningLetters'>U</span></Link>
+                <Link to={`/meanings/letter/${'V'}`} className='noLineLinks'><span className='meaningLetters'>V</span></Link>
+                <Link to={`/meanings/letter/${'W'}`} className='noLineLinks'><span className='meaningLetters'>W</span></Link>
+                <Link to={`/meanings/letter/${'X'}`} className='noLineLinks'><span className='meaningLetters'>X</span></Link>
+                <Link to={`/meanings/letter/${'Y'}`} className='noLineLinks'><span className='meaningLetters'>Y</span></Link>
+                <Link to={`/meanings/letter/${'Z'}`} className='noLineLinks'><span className='meaningLetters'>Z</span></Link>
                 </div>
             </div>
          );
--- dreamer/dreamer/src/components/Customer/Meanings/MeaningsByLetter.jsx
+++ dreamer/dreamer/src/components/Customer/Meanings/MeaningsByLetter.jsx
@@ -3,11 +3,49 @@ import React,{Component} from 'react';
 class MeaningsByLetter extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            meanings:[],
+         }
     }
+
+    //when component mounts/run inner function
+    componentDidMount(){
+        this.meaningsByLetter();
+    }
+
+    //function to fetch method to render all meanings that start with the letter that is passed in
+    meaningsByLetter = async() => {
+        let response = await fetch(`/api/meanings/view/${this.props.match.params.letter}`,{
+            method:"GET",
+            headers:{
+
+            }
+        })
+        let json = await response.json();
+        //sanity
+        console.log(json)
+        //place in state
+        this.setState({meanings:json})
+    }
+
     render() { 
         return ( 
-            <div>Reading Meanings By Letter</div>
+            <div>
+                <h4> "{this.props.match.params.letter}" Meanings</h4>
+                <div className='dreamersContainer'>
+                {
+                    this.state.meanings.map((meaning) => {
+                        return(
+                            <div key={meaning._id} className='dreamersDisplay'>
+                                <p className='listedData'>{meaning.word}</p>
+                                <p className='listedData'>{meaning.meaning}</p>
+                            </div>
+                        )
+                    })
+                }
+                </div>
+            </div>
+          
          );
     }
 }

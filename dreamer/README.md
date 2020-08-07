c89bbe8094433bd9051c15dcd88d4beb58a55a8e
MaryHooker <mc020791@yahoo.com>
2020-08-07 09:48:34 -0500

    deletion

--- dreamer/README.md
+++
@@ -1,1 +0,0 @@
-The Dreamer application is designed to give users the ability to record, save, post, learn and discuss dreams with like minded people in order get as much out of their dream life as one might their waking.
--- dreamer/server/config/keys.js
+++
@@ -1,8 +0,0 @@
-//File to hold Shared Connection String to MongoDB/ Secret Key for jwt/ Port NUmber
-
-module.exports = {
-    mongoURI: 
-    'mongodb+srv://Student:C0d3Cr3w@cluster0-p8fhv.mongodb.net/passion_project?retryWrites=true&w=majority',
-    secretOrKey: 'dr3am6uru9',
-    port: process.env.PORT ||1991
-}
